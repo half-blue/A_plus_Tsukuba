cb3681257ef0dc68b40bb8280a4eef6efe971a0a
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-11-25 01:34:22 +0900

    admin画面のカスタマイズ。検索ボックスや絞り込み条件を追加した。 #64

--- board/admin.py
+++ board/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from .models import Post, Thread, Reply, Subject, Notice
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Thread)
-admin.site.register(Reply)
-admin.site.register(Subject)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('sender_name','emotion','created_at')
+    ordering = ('-created_at',)
+    list_filter = ('emotion',)
+
+class ReplyModelAdmin(admin.ModelAdmin):
+    list_display = ('sender_name','emotion','created_at')
+    ordering = ('-created_at',)
+    list_filter = ('emotion',)
+
+class ThreadModelAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+class SubjectModelAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'teachers', 'schools', 'colleges')
+    list_display_links = ('code', 'name')
+    list_filter = ('schools', 'colleges')
+    search_fields = ['code', 'name', 'teachers']
+
+admin.site.register(Post, PostModelAdmin)
+admin.site.register(Thread, ThreadModelAdmin)
+admin.site.register(Reply, ReplyModelAdmin)
+admin.site.register(Subject, SubjectModelAdmin)
 admin.site.register(Notice)
