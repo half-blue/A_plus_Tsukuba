3114dd4658d4a36c04e73459b825f9f825e7d521
Takumi Tamura <tamutamuphone@gmail.com>
2023-03-05 16:16:01 +0900

    スクリプト改訂

---
+++ tools/docker.py
@@ -0,0 +1,24 @@
+# @nitoca用？便利スクリプト
+
+# 引数がpytestならば関数pytestを実行し、dbonlyならば関数dbonlyを実行する
+import subprocess
+import sys
+
+def pytest():
+    subprocess.run(["docker-compose", "up", "-d", "--build"])
+    subprocess.run(["docker", "exec", "a_plus_tsukuba-web", "pytest"])
+
+def dbonly():
+    subprocess.run(["docker-compose", "up", "-d", "--build"])
+    subprocess.run(["docker", "stop", "a_plus_tsukuba-web"])
+
+if __name__ == '__main__':
+    arg = sys.argv[1] if len(sys.argv) > 1 else ""
+    if arg == "pytest" :
+        print("Running pytest IN CONTAINER...")
+        pytest()
+    elif arg == "dbonly" :
+        print("Running the DB CONTAINER ONLY (stop the web container)...")
+        dbonly()
+    else:
+        print("argument error")
