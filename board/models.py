d0ba2e1be54d5264ed22a35724b43b019e9cdb4c
Takumi Tamura <tamutamuphone@gmail.com>
2023-04-06 01:02:29 +0900

    VARCHAR長さの変更

--- board/models.py
+++ board/models.py
@@ -3,18 +3,18 @@ from django.utils import timezone
 import uuid
 
 class Thread(models.Model):
-    title    = models.CharField(verbose_name='スレタイ',blank=False, null=False, max_length=150)
+    title    = models.CharField(verbose_name='スレタイ',blank=False, null=False, max_length=4096)
     def __str__(self):
         return self.title
 
 class Subject(models.Model):
     internal_id = models.AutoField(verbose_name="内部id", primary_key=True)
-    code = models.CharField(verbose_name="科目番号", max_length=10, default="")
-    name = models.CharField(verbose_name='講義名',blank=False, null=False, max_length=150)
-    teachers =  models.TextField(verbose_name='教員名', blank=True, max_length=100, default="")
-    subtype =  models.TextField(verbose_name='種類', blank=True, max_length=32, default="")
-    schools = models.CharField(verbose_name="学群等", max_length=40, default="", blank=True)
-    colleges = models.CharField(verbose_name="学類等", max_length=40, default="", blank=True)
+    code = models.CharField(verbose_name="科目番号", max_length=128, default="")
+    name = models.CharField(verbose_name='講義名',blank=False, null=False, max_length=4096)
+    teachers =  models.TextField(verbose_name='教員名', blank=True, max_length=4096, default="")
+    subtype =  models.TextField(verbose_name='種類', blank=True, max_length=1024, default="")
+    schools = models.CharField(verbose_name="学群等", max_length=1024, default="", blank=True)
+    colleges = models.CharField(verbose_name="学類等", max_length=1024, default="", blank=True)
     thread_id = models.ForeignKey(Thread, verbose_name="スレッドid", on_delete=models.CASCADE)
 
     def __str__(self):
