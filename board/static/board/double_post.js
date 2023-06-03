8b5741b19e9de5de7c7a10ce5add42f46a86e293
nitoca <78777225+nitoca@users.noreply.github.com>
2023-06-04 04:35:19 +0900

    二重投稿防止の汚い実装

--- board/static/board/double_post.js
+++ board/static/board/double_post.js
@@ -19,3 +19,27 @@ Vue.createApp({
     }
 
 }).mount('#review_Form_post_button');
+
+Vue.createApp({
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+            thread_id: document.getElementById('thread_id').value,
+            not_posted: true,
+        }
+    },
+    methods: {
+        loadreviwed() {
+            console.log("loadreviwed");
+            const reviewed_json = REVIEWED.getCookies();
+
+            if (this.thread_id in reviewed_json) {
+                this.not_posted = false;
+            }
+        },
+    },
+    mounted() {
+        this.loadreviwed();
+    }
+
+}).mount('#PostedBoolean');
