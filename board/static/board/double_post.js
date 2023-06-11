1dd4444a5075019b1e992ee0c6bbe7aea627af80
nitoca <78777225+nitoca@users.noreply.github.com>
2023-06-11 16:24:23 +0900

    みにリファクタリング

--- board/static/board/double_post.js
+++ board/static/board/double_post.js
@@ -6,17 +6,11 @@ Vue.createApp({
         }
     },
     methods: {
-        loadbookmark() {
-            const bookmark_json = REVIEWED.getCookies();
-        },
         onPostClick() {
             console.log("onPostClick");
             REVIEWED.setReviewed(this.thread_id);
         }
     },
-    mounted() {
-        this.loadbookmark();
-    }
 
 }).mount('#review_Form_post_button');
 
@@ -30,7 +24,6 @@ Vue.createApp({
     },
     methods: {
         loadreviwed() {
-            console.log("loadreviwed");
             const reviewed_json = REVIEWED.getCookies();
 
             if (this.thread_id in reviewed_json) {
