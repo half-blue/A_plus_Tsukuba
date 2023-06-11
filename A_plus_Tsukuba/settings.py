f7019adf1a9cf865e721dd502d9ced3986c31a30
ssf2xguile <iwanatteiwanai123@gmail.com>
2023-06-11 19:03:48 +0900

    投稿時にフラッシュメッセージを表示する これによりユーザーは投稿したフォームが正しく処理されていることを確認できる

--- A_plus_Tsukuba/settings.py
+++ A_plus_Tsukuba/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os, environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,4 +137,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DATABASES = {
     'default': env.db(),
+}
+
+# bootstrap5を使用するための設定
+MESSAGE_TAGS = {
+    messages.DEBUG: 'dark',
+    messages.ERROR: 'danger',
 }
