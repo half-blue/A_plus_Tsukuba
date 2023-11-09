04bd4ea3111b758e78943b0750933709da21a151
Takumi TAMURA <tamutamuphone@gmail.com>
2023-11-09 22:10:26 +0900

    impl: Review API

--- board/apis.py
+++ board/apis.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView, ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound, MethodNotAllowed
 from .serializers import *
 from .models import Post, Thread, Reply, Review
 from django.db.models import Avg
@@ -36,24 +37,50 @@ class post_replies(ListCreateAPIView):
     serializer_class = PostReplySerializer
 
 class get_subject_scores(APIView):
-    serializer_class = GetSubjectScoresSerializer
+
     def get(self, request, subcode, format=None):
         """
         Return 3 scores of specified subcode
         """
         
+        # 教科が存在するか確認
         try:
-            # FYI: MySQLでは大文字小文字を区別しない
-            thread_id = Subject.objects.filter(code = subcode).order_by("-year").values("thread_id")[0]["thread_id"]
+            ## FYI: MySQLでは大文字小文字を区別しない(照合順序によるけど)
+            subject = Subject.objects.filter(code = subcode).order_by("-year")[0]
         except IndexError:
-            self.serializer_class.is_valid(raise_exception=True)
+            raise NotFound("No such subject code")
         
-        ## 平均値を取得
+        # レビューが無効化されている場合はエラーを返す
+        if not subject.thread_id.enable_review:
+            raise MethodNotAllowed("GET", detail="Reviews are disabled for this subject")
+
+        # レビューを取得
+        thread_id = subject.thread_id.id
         reviews = Review.objects.filter(thread_id = thread_id)
-        average_ratings = reviews.aggregate(Avg('ratings_overall'), Avg('ratings_easiness'), Avg('ratings_content'))
+
+        # レビューが存在しない場合はNoneを返す
         count_ratings = reviews.count()
-
-        print(average_ratings, count_ratings)
-
-        self.serializer_class.is_valid(raise_exception=True)
-        return None
+        if count_ratings == 0:
+            serializer =  GetSubjectScoresSerializer(
+                {
+                    "subcode" : subcode,
+                    "average_ratings_overall": None,
+                    "average_ratings_easiness": None,
+                    "average_ratings_content": None,
+                    "count_ratings": 0,
+                }
+            )
+            return Response(serializer.data)
+        
+        # 平均値を取得
+        average_ratings = reviews.aggregate(Avg('ratings_overall'), Avg('ratings_easiness'), Avg('ratings_content'))
+        serializer =  GetSubjectScoresSerializer(
+                {
+                    "subcode" : subcode,
+                    "average_ratings_overall": average_ratings["ratings_overall__avg"],
+                    "average_ratings_easiness": average_ratings["ratings_easiness__avg"],
+                    "average_ratings_content": average_ratings["ratings_content__avg"],
+                    "count_ratings": count_ratings,
+                }
+            )
+        return Response(serializer.data)
