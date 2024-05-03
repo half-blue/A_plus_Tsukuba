1ba888f970d889b0fb00a6a5adf50e8e5ccda5e5
Takumi Tamura <tamutamuphone@gmail.com>
2024-05-03 19:10:10 +0900

    fix data

--- board/views.py
+++ board/views.py
@@ -220,10 +220,11 @@ def assetlinks_json(request):
         "relation": ["delegate_permission/common.handle_all_urls"],
         "target": {
             "namespace": "android_app",
-            "package_name": "com.example",
+            "package_name": "com.aplus.tsukuba2023",
             "sha256_cert_fingerprints": [
-                "14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5"
+                "64:2C:3A:D4:5D:A9:F7:E4:83:5B:F6:1C:49:0C:95:42:E6:A1:1C:E5:9D:F9:25:DA:E0:38:B5:81:6C:19:B7:1B"
             ]
         }
     }]
+    ## `safe=False`は今日においてはセキュリティ上の問題とはならないらしい
     return JsonResponse(data, safe=False)
